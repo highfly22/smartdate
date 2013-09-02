b5312ade53ed241a0d6fed55f7d941d33e0481fe
Haiwei Zhou <haiwei.zhou@intel.com>
2013-09-02 12:00:31 +0800

    try to define FunBox

--- typeclass.hs
+++ typeclass.hs
@@ -1,12 +1,51 @@
 {-# LANGUAGE ExistentialQuantification, DeriveDataTypeable #-}
 import Data.Typeable;
+import Data.Maybe;
 
-data EnumBox = forall s. (Enum s, Show s) => EB s
+class Typeable a => XTerm a where
+  xapply :: (XTerm b, Typeable b, XTerm r, Typeable r) => r -> a -> b -> Maybe r
+
+data FunBox = forall a b. (XTerm a, XTerm b) => FB (a -> b)
+            deriving Typeable
+
+i2c :: Int -> Char
+i2c = (toEnum :: Int -> Char)
+
+instance XTerm FunBox where
+  xapply r (FB a) b = Nothing
+
+instance XTerm Int where
+  xapply r a b = Nothing
+
+instance XTerm Char where
+  xapply r a b = Nothing
+
+class XEnum a where
+  xsucc :: a -> a
+  xpred :: a -> a
+  xenumFrom :: a -> [a]
+
+instance XEnum Int where
+  xsucc = succ
+  xpred = pred
+  xenumFrom = enumFrom
+
+instance XEnum Char where
+  xsucc = succ
+  xpred = pred
+  xenumFrom = enumFrom
+
+data EnumBox = forall s. (XTerm s, XEnum s, Show s) => EB s
                deriving Typeable
 
 instance Show EnumBox where
   show (EB s) = "EB " ++ show s
 
-next :: EnumBox -> EnumBox
-next (EB a) = succ a
+instance XEnum EnumBox where
+  xsucc (EB a) = EB . xsucc $ a
+  xpred (EB a) = EB . xpred $ a
+  xenumFrom (EB a) = foldr (\a b -> EB a : b) [] (xenumFrom a)
+
+instance XTerm EnumBox where
+  xapply r a b = Nothing 
 
